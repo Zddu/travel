620a9e759a4a3637b640587e7ec396944fc669af
Zddu <1501815524@qq.com>
2021-04-06 19:35:13 +0800

    '城市列表功能'

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -2,16 +2,21 @@
   <div>
     <city-header></city-header>
     <city-search></city-search>
+    <city-list></city-list>
+    <city-alphabet></city-alphabet>
   </div>
 </template>
 
 <script>
-    import CityHeader from "@/components/city/header/Header";
-    import CitySearch from "@/components/city/Search/Search";
-    export default {
-        name: "City",
-      components: {CitySearch, CityHeader}
-    }
+  import CityHeader from "@/components/city/header/Header";
+  import CitySearch from "@/components/city/Search/Search";
+  import CityList from "@/components/city/listview/ListView";
+  import CityAlphabet from "@/components/city/alphabet/Alphabet";
+
+  export default {
+    name: "City",
+    components: {CityAlphabet, CitySearch, CityHeader, CityList}
+  }
 </script>
 
 <style scoped lang="stylus">
