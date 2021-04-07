0b6b424a142367755de4d42fda096809acfb08cf
Zddu <1501815524@qq.com>
2021-04-07 15:48:15 +0800

    '景点详情头部'

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,19 +1,24 @@
 <template>
   <div>
+    <detail-header></detail-header>
     <detail-banner></detail-banner>
+    <div class="content"></div>
   </div>
 </template>
 
 <script>
 
   import DetailBanner from "@/components/deatil/Banner";
+  import DetailHeader from "@/components/deatil/Header";
 
   export default {
     name: "Detail",
-    components: {DetailBanner}
+    components: {DetailHeader, DetailBanner}
   }
 </script>
 
 <style scoped lang="stylus">
+  .content
+    height 50rem
 
 </style>
